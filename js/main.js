e46cba4b2e25256f2b6eb5274c97a7f806281d88
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-06-05 09:11:54 +0300

    refactor hovering event

--- js/main.js
+++ js/main.js
@@ -1,29 +1,11 @@
-const image1 = document.querySelector('.image1')
-const image2 = document.querySelector('.image2')
-const image3 = document.querySelector('.image3')
-const image4 = document.querySelector('.image4')
+const images = document.querySelectorAll('figure')
 
-image1.addEventListener('mouseover', (e)=>{
-    image1.style.border = "2px solid yellow"
-})
-image1.addEventListener('mouseleave', (e)=>{
-    image1.style.border = ""
-})
-image2.addEventListener('mouseover', (e)=>{
-    image2.style.border = "2px solid orange"
-})
-image2.addEventListener('mouseleave', (e)=>{
-    image2.style.border = ""
-})
-image3.addEventListener('mouseover', (e)=>{
-    image3.style.border = "2px solid white"
-})
-image3.addEventListener('mouseleave', (e)=>{
-    image3.style.border = ""
-})
-image4.addEventListener('mouseover', (e)=>{
-    image4.style.border = "2px solid lime"
-})
-image4.addEventListener('mouseleave', (e)=>{
-    image4.style.border = ""
+images.forEach((image)=>{
+    image.addEventListener('mouseover', ()=>{
+        image.style.border = "2px solid white"
+    })
+
+    image.addEventListener('mouseleave', ()=>{
+        image.style.border = ""
+    })
 })
